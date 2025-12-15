029a0b9f36da041bef0b4366f3ae22dc4b0232ca
semik-serma <semikserma@gmail.com>
2025-12-15 19:45:11 +0545

    hello vsreact

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+'use client'
+import Navbar from "@/components/Navbar"
+
+const page = () => {
+  return (
+    <div>
+      
+      <Navbar/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+'use client'
+import Link from 'next/navigation'
+import Image from 'next/image'
+
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div></div>
+            <div>
+        
+      
+        <Link href="/">Home</Link>
+        <Link href="/">About</Link>
+        <Link href="/">Contact</Link>
+      
+    
+            </div>
+        </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Navbar
